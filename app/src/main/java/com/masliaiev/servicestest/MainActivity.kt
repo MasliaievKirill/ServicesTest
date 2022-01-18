9596721d24e44ca4c88804d2f4ce5716c9f4e365
MasliaievKirill <kirillmaslyaev@gmail.com>
2022-01-18 15:51:25 +0200

    1.MyService 2.MyForegroundService 3.MyIntentService 4.MyJobService

--- app/src/main/java/com/masliaiev/servicestest/MainActivity.kt
+++ app/src/main/java/com/masliaiev/servicestest/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.masliaiev.servicestest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import com.masliaiev.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this))
+        }
+        binding.foregroundService.setOnClickListener {
+            startForegroundService(MyForegroundService.newIntent(this))
+        }
+        binding.intentService.setOnClickListener {
+            startForegroundService(MyIntentService.newIntent(this))
+        }
+        binding.jobScheduler.setOnClickListener {
+            val componentName = ComponentName(this, MyJobService::class.java)
+            val jobInfo = JobInfo.Builder(MyJobService.JOB_ID, componentName)
+//                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build()
+            val jobScheduler = getSystemService(JOB_SCHEDULER_SERVICE) as JobScheduler
+            jobScheduler.schedule(jobInfo)
+        }
+
     }
+
+
 }
